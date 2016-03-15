22f65df221e596eb1efa2beeff31c223d77752b8
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-03-15 10:27:36 +0100

    Added exercise 16

---
+++ Exercise16/Exercise16/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Exercise16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+
+            ArrayList arrLst = new ArrayList();
+
+            sw.Start();
+            for (int i = 0; i < 10000000; i++)
+            {
+                arrLst.Add(i);
+            }
+            sw.Stop();
+
+            Console.WriteLine("Array list:" + sw.ElapsedMilliseconds + "miliseconds");
+            sw.Reset();
+
+            List<int> myList = new List<int>();
+
+            sw.Start();
+            for (int i = 0; i < 10000000; i++)
+            {
+                myList.Add(i);
+            }
+            sw.Stop();
+
+            Console.WriteLine("Generic list:" + sw.ElapsedMilliseconds + "miliseconds");
+            sw.Reset();
+
+
+
+        }
+    }
+}
