b46015aa2bd1997a2c29fc66f14ffc0edefc6c29
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-03-18 21:18:45 +0100

    added exercise 26

---
+++ Exercise26/Exercise26/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise26
+{
+    class Calculator
+    {
+        public static int Memory { get; private set; } 
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static void Add(int a)
+        {
+            Memory += a;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public static void Subtract(int a)
+        {
+            Memory -= a;
+        }
+    }
+}
---
+++ Exercise26/Exercise26/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculator.Add(5, 5));
+            Console.WriteLine(Calculator.Subtract(15, 10));
+
+            Console.WriteLine("memory before: " + Calculator.Memory);
+            Calculator.Add(20);
+            Calculator.Add(10);
+            Calculator.Subtract(5);
+            Calculator.Subtract(10);
+            Console.WriteLine("memory after: " + Calculator.Memory);
+        }
+    }
+}
