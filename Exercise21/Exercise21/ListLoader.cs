7ed8511367035fdb17713bea5ac347eb4f5ed3f5
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-03-18 17:37:28 +0100

    finnished exercise 20

---
+++ Exercise21/Exercise21/ListLoader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise21
+{
+    class ListLoader
+    {
+        public List<LanguageData> LoadData()
+        {
+            List<LanguageData> result = new List<LanguageData>();
+            StreamReader reader = new StreamReader("languageData.txt");
+
+            string line = "";
+
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                var name = line.Split('\t')[0];//splits at the tabs in the file, creates an array of strings, and then takes the first index
+                var year = int.Parse(line.Split('\t')[1]);//splits at the tabs in the file, creates an array of strings, and then takes the second index
+                var description = line.Split('\t')[2];//splits at the tabs in the file, creates an array of strings, and then takes the third index
+
+                var langData = new LanguageData
+                {
+                    Description = description,
+                    Name = name,
+                    Year = year
+                };
+                result.Add(langData);
+            }
+
+            reader.Close();
+            return result;
+        }
+
+        public void SaveToFile(List<LanguageData>files)
+        {
+            StreamWriter writer = new StreamWriter("filtered.txt");
+
+            foreach (var item in files.OrderBy(x => x.Year))//TODO study linq/lambda
+            {
+                writer.WriteLine($"{item.Name}-{item.Year} ({item.Description})");
+            }
+            writer.Close();
+        }
+    }
+}
---
+++ Exercise21/Exercise21/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListLoader loader = new ListLoader();
+            var myData = loader.LoadData();
+            Console.Write("start year: ");
+            int startYear = int.Parse(Console.ReadLine());
+            Console.Write("end year: ");
+            int endYear = int.Parse(Console.ReadLine());
+
+
+            var filteredResult = new List<LanguageData>();//setting up list where later store filtered result
+            foreach (var item in myData)
+            {
+                if (item.Year >= startYear && item.Year <= endYear)
+                    filteredResult.Add(item);
+            }
+
+            foreach(var item in filteredResult.OrderBy(x => x.Year))//TODO study linq/lambda
+            {
+                Console.WriteLine($"{item.Name}\t{item.Year}\t{item.Description}");
+            }
+
+
+            loader.SaveToFile(filteredResult);//writing whatever is on the filteredResult list to file
+
+
+
+        }
+    }
+}
