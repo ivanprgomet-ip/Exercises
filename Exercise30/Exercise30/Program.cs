91a534cbb95f17326e785c578aa22cb6212369e0
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-03-19 11:44:19 +0100

    added exercise 30

---
+++ Exercise30/Exercise30/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weather forecast = new Weather();
+
+            Console.WriteLine("Enter mm: ");
+            int inMonth= int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter DD: ");
+            int inDay = int.Parse(Console.ReadLine());
+            DateTime inputDate = new DateTime(DateTime.Now.Year, inMonth, inDay);
+
+            /*To access enum weather type, 
+            must go via weather class*/
+            Weather.Type forecastResults = forecast.Forecast(inputDate);
+            Console.WriteLine($"Forecast results: {forecastResults}");
+        }
+    }
+}
---
+++ Exercise30/Exercise30/Weather.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise30
+{
+    class Weather
+    {
+        public enum Type
+        {
+            Sunny,
+            Cloudy,
+            Rainy,
+            Stormy,
+            Windy,
+            Misty,
+            Snowy,
+            Icy,
+        }
+        public Type Forecast(DateTime date)
+        {
+            //random logic to calculate forecast
+            Type myForecast = Type.Sunny;
+
+            if (date.Month <= 12 && date.Month >= 10)
+                myForecast = Type.Snowy;
+            if (date.Month < 10 && date.Month >= 8)
+                myForecast = Type.Rainy;
+            if (date.Month < 8 && date.Month >= 6)
+                myForecast = Type.Sunny;
+            if (date.Month < 6 && date.Month >= 3)
+                myForecast = Type.Misty;
+            if (date.Month < 3 && date.Month >= 1)
+                myForecast = Type.Stormy;
+            return myForecast;
+        }
+    }
+}
